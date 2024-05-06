2b20f6b11041bfa4494f3f0b401357ca51b73cf7
NStrategy <134973005+NStrategy@users.noreply.github.com>
2024-05-06 21:06:04 +0200

    Exclude Dispatch From Stealth-Windows If BtE Is Castable
    Sometimes, the addon would suggest Dispatch near the end of a Stealth-Window although is BtE ready. With this change, this should not happen anymore. 
    Often times, you would still be able to hit the BtE within the window. I cant quite pinpoint as to why that happens, but it sometimes did.

--- HeroRotation_Rogue/Outlaw.lua
+++ HeroRotation_Rogue/Outlaw.lua
@@ -480,13 +480,12 @@ local function Stealth()
 
   -- # High priority Between the Eyes for Crackshot, except not directly out of Shadowmeld
 	-- actions.stealth+=/between_the_eyes,if=variable.finish_condition&talent.crackshot&(!buff.shadowmeld.up|stealthed.rogue)
-	if S.BetweentheEyes:IsCastable() and Target:IsSpellInRange(S.BetweentheEyes) and (not Player:BuffUp(S.Shadowmeld) or Player:BuffUp(S.SubterfugeBuff) or Player:BuffUp(S.ShadowDanceBuff) or Player:BuffUp(S.VanishBuff) or Player:BuffUp(S.VanishBuff2) or Player:BuffUp(S.Stealth) or Player:BuffUp(S.Stealth2))
-    and Finish_Condition() and S.Crackshot:IsAvailable() then
+	if S.BetweentheEyes:IsCastable() and Target:IsSpellInRange(S.BetweentheEyes) and Finish_Condition() and S.Crackshot:IsAvailable() and (not Player:BuffUp(S.Shadowmeld) or Player:StealthUp(true, false)) then
     if HR.CastPooling(S.BetweentheEyes) then return "Cast Between the Eyes" end
    end
 
 	-- actions.stealth+=/dispatch,if=variable.finish_condition
-	if S.Dispatch:IsCastable() and Target:IsSpellInRange(S.Dispatch) and Finish_Condition() then
+	if S.Dispatch:IsCastable() and Target:IsSpellInRange(S.Dispatch) and Finish_Condition() and not S.BetweentheEyes:IsCastable() then
 		if HR.CastPooling(S.Dispatch) then return "Cast Dispatch" end
 	end
 
